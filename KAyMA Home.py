dfe55ad734a4b62f95b90b590f0f6ecca99dbfac
Manuel Izquierdo <mizq14@gmail.com>
2026-01-11 22:10:01 +1100

    Update KAyMA Home.py

--- KAyMA Home.py
+++ KAyMA Home.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import date, timedelta
-from utils import (
+from utils.utils import (
     fetch_data, 
     calculate_indicators, 
     preparar_datos_markov,
