7e5efb4c1f8b423dbbb1f0390105f6ef2f1d6f02
Manuel Izquierdo <mizq14@gmail.com>
2026-01-11 22:12:03 +1100

    Update 03 Trend Wkly Donchian.py

--- pages/03 Trend Wkly Donchian.py
+++ pages/03 Trend Wkly Donchian.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 import warnings
-from utils import check_password
+from utils.utils import check_password
 
 warnings.filterwarnings('ignore')
 
