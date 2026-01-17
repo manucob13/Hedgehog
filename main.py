edec670777fa8a99f4c503066719113136fc4964
Manuel Izquierdo <mizq14@gmail.com>
2026-01-17 11:51:42 +1100

    Update main.py

--- main.py
+++ main.py
@@ -19,25 +19,25 @@ st.set_page_config(
 # 2. DEFINICIÓN DE LAS PÁGINAS
 # --- Sección ESTRATEGIAS TE ---
 te_signals = st.Page(
-    "Estrategias /TE/00 TE Signals.py", 
+    "Estrategias/TE/00 TE Signals.py", 
     title="Signals", 
     icon="🦔", 
     default=True
 )
 te_calculos = st.Page(
-    "Estrategias /TE/01 TE Calculos.py", 
+    "Estrategias/TE/01 TE Calculos.py", 
     title="Cálculos", 
     icon="🦔"
 )
 
 # --- Sección ESTRATEGIAS TC ---
 tc_signals = st.Page(
-    "Estrategias /TC/00 TC Signals.py", 
+    "Estrategias/TC/00 TC Signals.py", 
     title="Signals", 
     icon="🦉"
 )
 tc_calculos = st.Page(
-    "Estrategias /TC/01 TC Calculos.py", 
+    "Estrategias/TC/01 TC Calculos.py", 
     title="Cálculos", 
     icon="🦉"
 )
@@ -64,7 +64,7 @@ gex_analyzer = st.Page(
     icon="🧪"
 )
 screener_risk = st.Page(
-    "Herramientas/05 Screener Ext Risk.py", 
+    "Herramientas/05 Screener Ext. Risk.py", 
     title="Screener Ext. Risk", 
     icon="🚨"
 )
