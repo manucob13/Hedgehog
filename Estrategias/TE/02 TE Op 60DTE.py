c5e1a94bd377e25118f8f795c3ed076591cd0f14
Manuel Izquierdo <mizq14@gmail.com>
2026-06-11 17:33:14 +1000

    Update 02 TE Op 60DTE.py

--- Estrategias/TE/02 TE Op 60DTE.py
+++ Estrategias/TE/02 TE Op 60DTE.py
@@ -1,7 +1,3 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-
 import streamlit as st
 import pandas as pd
 from datetime import date, timedelta, datetime
